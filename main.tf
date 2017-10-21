5a805e38fea77d379fe01b75a9253656c9a848c0
Kingdon Barrett <kbarret8@nd.edu>
2017-10-21 11:57:26 -0400

    set a variable for port

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
   tags {
@@ -18,9 +18,12 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
 }
