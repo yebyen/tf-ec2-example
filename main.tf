bfc98275578e251fdc8ec394c9d61ab101338f65
Kingdon Barrett <kbarret8@nd.edu>
2017-10-21 11:55:36 -0400

    add a security group

--- main.tf
+++ main.tf
@@ -15,3 +15,12 @@ resource "aws_instance" "example" {
     Name = "terraform-example"
   }
 }
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
