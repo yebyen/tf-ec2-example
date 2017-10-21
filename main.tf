b31cbd03034d0622b9dffcbd7b4ffe30c91b0364
Kingdon Barrett <kbarret8@nd.edu>
2017-10-21 11:54:31 -0400

    add an HTTP server

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ resource "aws_instance" "example" {
   ami = "ami-2d39803a"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags {
     Name = "terraform-example"
   }
