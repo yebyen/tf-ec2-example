b85a0d8a575a602439e208f8579ada6d9a28c484
Kingdon Barrett <kbarret8@nd.edu>
2017-10-21 16:22:09 -0400

    (green) working with t2.micro and my VPC example

--- main.tf
+++ main.tf
@@ -1,18 +1,17 @@
-provider "aws" {
-  region = "us-east-1"
-}
+provider "aws" { }
 resource "aws_key_pair" "yebyen" {
   key_name   = "yebyen-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDib2Mn2TVPncZW9iivWH13ZkTIMUaKPhJQp7QFYrTZtnoCiaq9pINgmW6LoGEcNdLBctOH3/aDVW3Js+AGjwJJns0/rR/G9TCL/DcFCpWH5FmOgt5KRXU+IPmNKbbxMuGXJH2khWI+crX8QkTaFPZpAeugHdVgCuK4rcblWDDnqX7McH4tgEAt/Oe+iTPEwk3DtosRKl79p6FIUo0JMQYMYErgae6e1c4n/h1Pg8EPCfGdaVcqeIfHoEisJHUKHPsmQ3JUf2LQyEM5K4/Kx4YH+Xz3KrI/2Bs7CXq+8/aw0jlnnSmgQ13VUdhp1wNpOErWAhTtcV1DF9TeqDgsbjEX kingdonb@kbarret8-mbpro-2014.dhcp.nd.edu"
 }
 resource "aws_instance" "example" {
   ami = "ami-2d39803a"
-  instance_type = "m3.medium"
+  instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
   key_name = "yebyen-key"
+  subnet_id = "subnet-fac1dba0"
 
   user_data = <<-EOF
-              #!/bin/bash
+              #!/bin/bash -x
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
@@ -20,9 +19,13 @@ resource "aws_instance" "example" {
   tags {
     Name = "terraform-example"
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
+  vpc_id = "vpc-8fcc11f7"
   # TCP access
   ingress {
     from_port = 0
@@ -37,8 +40,14 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   default = 8080
+}
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
